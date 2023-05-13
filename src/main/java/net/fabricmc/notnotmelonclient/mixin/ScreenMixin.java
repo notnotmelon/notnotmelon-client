53d3fbb6d8271764d7979164015b619ea8d60a8e
Sm^HpJk4 <zacharypicco@gmail.com>
2023-05-12 21:01:38 -0500

    make scrollable tooltips only work on items

--- src/main/java/net/fabricmc/notnotmelonclient/mixin/ScreenMixin.java
+++ src/main/java/net/fabricmc/notnotmelonclient/mixin/ScreenMixin.java
@@ -8,17 +8,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.fabricmc.notnotmelonclient.misc.ScrollableTooltips;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
 	@ModifyVariable(method = "renderTooltipFromComponents(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", ordinal = 6, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE))
 	public int modifyXOffset(int x) {
-		return x + ScrollableTooltips.x;
+		if ((Object) this instanceof HandledScreen)
+			return x + ScrollableTooltips.x;
+		return x;
 	}
 
     @ModifyVariable(method = "renderTooltipFromComponents(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", ordinal = 7, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE))
 	public int modifyYOffset(int y) {
-		return y + ScrollableTooltips.y;
+		if ((Object) this instanceof HandledScreen)
+			return y + ScrollableTooltips.y;
+		return y;
 	}
 
     @Inject(method = "close", at = @At("HEAD"))
