2340ee19c638ff9dae4a8bb4c2d6cc657c97d85c
notnotmelon <zacharypicco@gmail.com>
2023-05-24 20:29:03 -0500

    Fix configs

--- src/main/java/net/fabricmc/notnotmelonclient/config/JsonLoader.java
+++ src/main/java/net/fabricmc/notnotmelonclient/config/JsonLoader.java
@@ -2,7 +2,10 @@ package net.fabricmc.notnotmelonclient.config;
 
 import dev.isxander.yacl.config.GsonConfigInstance;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.notnotmelonclient.Main;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JsonLoader {
@@ -10,6 +13,12 @@ public class JsonLoader {
     public static Path configDir;
     public static void load() {
         configDir = FabricLoader.getInstance().getConfigDir().resolve("notnotmelonclient/config.properties");
+        try {
+            Files.createDirectories(configDir);
+        } catch (IOException e) {
+            Main.LOGGER.error("Failed to create config dir", e);
+        }
+
         jsonInterface = new GsonConfigInstance<Config>(Config.class, configDir);
         jsonInterface.load();
     }
