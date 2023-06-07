749e6eb32fd0a564122c756100b9009bc19ce53d
notnotmelon <zacharypicco@gmail.com>
2023-06-07 00:03:39 -0500

    itemlist rendering improvement

--- src/main/java/net/fabricmc/notnotmelonclient/itemlist/ItemList.java
+++ src/main/java/net/fabricmc/notnotmelonclient/itemlist/ItemList.java
@@ -1,13 +1,16 @@
 package net.fabricmc.notnotmelonclient.itemlist;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.notnotmelonclient.Main;
 import net.fabricmc.notnotmelonclient.config.Config;
 import net.fabricmc.notnotmelonclient.misc.ScrollableTooltips;
 import net.fabricmc.notnotmelonclient.util.RenderUtil;
-import net.fabricmc.notnotmelonclient.util.Util;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.awt.*;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
 import static net.fabricmc.notnotmelonclient.Main.client;
 
 public class ItemList {
+	private static final Identifier ITEMLIST = new Identifier(Main.NAMESPACE, "textures/gui/itemlist.png");
 	public static int xOffset = 0;
 	public static int yOffset = 18;
 	public static int lastMouseX = -1;
@@ -28,18 +32,28 @@ public class ItemList {
 	public static int textRenderX;
 
 	public static void render(HandledScreen<?> screen, MatrixStack matrices, int mouseX, int mouseY) {
+		pageNumber = 1;
 		if (!NeuRepo.isDownloaded) return;
 		ItemRenderer itemRenderer = client.getItemRenderer();
-		mouseX -= xOffset;
-		int targetMouseX = mouseX - Math.abs(mouseX) % step + xOffset;
+		int offsetMouseX = mouseX - xOffset;
+		int targetMouseX = offsetMouseX - Math.abs(offsetMouseX) % step + xOffset;
 		int targetMouseY = mouseY - Math.abs(mouseY) % step;
 		boolean renderedTooltip = false;
 
 		for (int i = startIndex; i < endIndex; i++) {
 			ItemListIcon icon = NeuRepo.itemListIcons.get(i);
-			itemRenderer.renderInGui(matrices, icon.stack, icon.x, icon.y);
-			if (!renderedTooltip && targetMouseX == icon.x && targetMouseY == icon.y) {
-				screen.renderTooltip(matrices, screen.getTooltipFromItem(icon.stack), icon.stack.getTooltipData(), mouseX + xOffset, mouseY);
+			int x = icon.x;
+			int y = icon.y;
+			itemRenderer.renderInGui(matrices, icon.stack, x, y);
+			if (icon.children != null) {
+				matrices.push();
+				matrices.translate(0, 0, 200);
+				RenderSystem.setShaderTexture(0, ITEMLIST);
+				DrawableHelper.drawTexture(matrices, x + 12, y + 4, 0, 0, 7, 11, 54, 40);
+				matrices.pop();
+			}
+			if (!renderedTooltip && targetMouseX == x && targetMouseY == y) {
+				screen.renderTooltip(matrices, screen.getTooltipFromItem(icon.stack), icon.stack.getTooltipData(), mouseX, mouseY);
 				renderedTooltip = true;
 			}
 		}
@@ -49,6 +63,21 @@ public class ItemList {
 		lastMouseY = targetMouseY;
 
 		RenderUtil.drawCenteredText(matrices, client, textRenderX, 7, pageNumberText, -1);
+		RenderSystem.setShaderTexture(0, ITEMLIST);
+		drawArrows(matrices, mouseX, mouseY);
+	}
+
+	public static void drawArrows(MatrixStack matrices, int mouseX, int mouseY) {
+		int width = 7;
+		int height = 11;
+		int x = textRenderX - 21 - width;
+		int xx = textRenderX + 20;
+		int y = 5;
+		int v = x <= mouseX && mouseX <= x + width && y <= mouseY && mouseY <= y + height ? height : 0;
+		int vv = xx <= mouseX && mouseX <= xx + width && y <= mouseY && mouseY <= y + height ? height : 0;
+
+		DrawableHelper.drawTexture(matrices, x, y, 0, v, width, height, 54, 40);
+		DrawableHelper.drawTexture(matrices, xx, y, width, vv, width, height, 54, 40);
 	}
 
 	public static void onOpenScreen(HandledScreen<?> screen) {
@@ -72,7 +101,6 @@ public class ItemList {
 			if (!rectangle.contains(x, y)) {
 				if (!freezePageSize) pageSize++;
 				icons.get(i).setLocation(x + xOffset, y);
-				if (icons.get(i).children != null) Util.print(icons.get(i).stack.getName());
 				i++;
 			}
 			x += step;
