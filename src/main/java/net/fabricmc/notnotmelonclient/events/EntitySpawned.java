2eaa641e671b3a7bd2a74e1d99d809eb9bfa1fb6
notnotmelon <zacharypicco@gmail.com>
2023-06-14 08:33:27 -0500

    a

--- src/main/java/net/fabricmc/notnotmelonclient/events/EntitySpawned.java
+++ src/main/java/net/fabricmc/notnotmelonclient/events/EntitySpawned.java
@@ -2,14 +2,15 @@ package net.fabricmc.notnotmelonclient.events;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
+import net.fabricmc.notnotmelonclient.util.Scheduler;
 import net.minecraft.entity.LivingEntity;
 
 public interface EntitySpawned {
 	void onEntitySpawned(LivingEntity entity);
 
 	Event<EntitySpawned> EVENT = EventFactory.createArrayBacked(EntitySpawned.class,
-		(listeners) -> (LivingEntity entity) -> {
+		(listeners) -> (LivingEntity entity) -> Scheduler.schedule(() -> {
 			for (EntitySpawned listener : listeners)
 				listener.onEntitySpawned(entity);
-		});
+		}, 1));
 }
